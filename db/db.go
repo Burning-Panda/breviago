b111038bda829cc3e018f06351616e953b0f6c4e
André <31665764+Burning-Panda@users.noreply.github.com>
2025-03-25 22:33:11 +0100

    Add GET, POST and PUT methods

--- db/db.go
+++ db/db.go
@@ -36,16 +36,75 @@ func GetDB() *sql.DB {
 	return database
 }
 
+type Acronym struct {
+	ID          int    `json:"id"`
+	ShortForm   string `json:"short_form"`
+	LongForm    string `json:"long_form"`
+	Description string `json:"description"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+type Category struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type Label struct {
+	ID          int    `json:"id"`
+	Label       string `json:"label"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
 // initSchema creates the necessary tables if they don't exist
 func initSchema() {
 	createTableSQL := `
 	CREATE TABLE IF NOT EXISTS acronyms (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		short_form TEXT NOT NULL UNIQUE,
+		short_form TEXT NOT NULL,
 		long_form TEXT NOT NULL,
+		description TEXT,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		
+		UNIQUE(short_form, long_form)
+	);
+	CREATE TABLE IF NOT EXISTS acronym_categories (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		acronym_id INTEGER NOT NULL,
+		category_id INTEGER NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+		FOREIGN KEY (acronym_id) REFERENCES acronyms(id) ON DELETE CASCADE,
+		FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+	);
+	CREATE TABLE IF NOT EXISTS categories (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL UNIQUE,
+		description TEXT,
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 		updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
-	);`
+	);
+	CREATE TABLE IF NOT EXISTS note_joiner (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		acronym_id INTEGER,
+		note_id INTEGER NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		
+		FOREIGN KEY (acronym_id) REFERENCES acronyms(id) ON DELETE SET NULL,
+		FOREIGN KEY (note_id) REFERENCES notes(id) ON DELETE CASCADE
+	);
+	CREATE TABLE IF NOT EXISTS notes (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		note TEXT NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	);
+	`
 
 	_, err := database.Exec(createTableSQL)
 	if err != nil {
@@ -56,6 +115,7 @@ func initSchema() {
 	walMode := database.QueryRow("PRAGMA journal_mode")
 	var mode string
 	err = walMode.Scan(&mode)
+	
 	if err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Burning-Panda/acronyms-vault/db"
 
@@ -24,9 +25,10 @@ func main() {
 	r := gin.Default()
 
 	// Middleware
-	
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
+	r.Use(func(ctx *gin.Context) {
+		// TODO: Add Authentication middleware here
+		ctx.Next()
+	})
 
 	r.GET("/", getDefault)
 
@@ -64,7 +66,7 @@ func getDefault(c *gin.Context) {
 
 func getAcronyms(c *gin.Context) {
 	database := db.GetDB()
-	rows, err := database.Query("SELECT id, short_form, long_form, created_at, updated_at FROM acronyms")
+	rows, err := database.Query("SELECT id, short_form, long_form, created_at, updated_at FROM acronyms ORDER BY short_form DESC")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch acronyms"})
 		return
@@ -75,7 +77,9 @@ func getAcronyms(c *gin.Context) {
 	for rows.Next() {
 		var id int
 		var shortForm, longForm, createdAt, updatedAt string
-		if err := rows.Scan(&id, &shortForm, &longForm, &createdAt, &updatedAt); err != nil {
+
+		err := rows.Scan(&id, &shortForm, &longForm, &createdAt, &updatedAt);
+		if  err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan acronym"})
 			return
 		}
@@ -92,15 +96,110 @@ func getAcronyms(c *gin.Context) {
 }
 
 func createAcronym(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
+
+	var acronym = db.Acronym{
+		ShortForm: "",
+		LongForm: "",
+		Description: "",
+	}
+
+	if err := c.ShouldBindJSON(&acronym); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	db := db.GetDB()
+
+	res, err := db.Exec(
+		"INSERT INTO acronyms (short_form, long_form, description) VALUES (?, ?, ?)",
+		acronym.ShortForm,
+		acronym.LongForm,
+		acronym.Description,
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create acronym"})
+		return
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get last insert id"})
+		return
+	}
+
+	db.QueryRow(
+		"SELECT id, short_form, long_form, description FROM acronyms WHERE id = ?", id,
+	).Scan(
+		&acronym.ID,
+		&acronym.ShortForm,
+		&acronym.LongForm,
+		&acronym.Description,
+	)
+
+	c.JSON(http.StatusOK, acronym)
 }
 
 func updateAcronym(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
+	id := c.Param("id")
+
+	i, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+
+	var acronym = db.Acronym{
+		ID: i,
+	}
+
+	if err := c.ShouldBindJSON(&acronym); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	database := db.GetDB()
+
+	res, err := database.Exec(
+		"UPDATE acronyms SET short_form = ?, long_form = ?, description = ? WHERE id = ?",
+		acronym.ShortForm,
+		acronym.LongForm,
+		acronym.Description,
+		id,
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update acronym"})
+		return
+	}
+
+	lastInsert, liErr := res.LastInsertId()
+
+	if liErr != nil {
+		c.JSON(http.StatusAccepted, gin.H{"error": "Acronym was updated, but it was not returned"})
+	}
+
+	var returnedAcronym = db.Acronym{
+		ID: int(lastInsert),
+	}
+
+	err = database.QueryRow(
+		"SELECT * FROM acronyms WHERE id = ?",
+		lastInsert,
+	).Scan(
+		&returnedAcronym.ID,
+		&returnedAcronym.ShortForm,
+		&returnedAcronym.LongForm,
+		&returnedAcronym.Description,
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch updated acronym"})
+		return
+	}
+
+	c.JSON(http.StatusOK, returnedAcronym)
 }
 
 func deleteAcronym(c *gin.Context) {
